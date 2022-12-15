fb88c6e51a649c80cc563674d29590f19de148f9
im-snowin <hello.snowin@gmail.com>
2022-12-15 22:25:32 +0530

    created middlewares to protect user from signin without login and created user routes

---
+++ src/handlers/user.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import prisma from '../prisma';
+import { comparePassword, createJWT, hashPassword } from '../utils/auth';
+
+export const createNewUser = async (req: Request, res: Response) => {
+	const user = await prisma.user.create({
+		data: {
+			username: req.body.username,
+			password: await hashPassword(req.body.password),
+		},
+	});
+
+	const token = createJWT(user);
+	res.json({ token });
+};
+
+export const signIn = async (req: Request, res: Response) => {
+	const user = await prisma.user.findUnique({
+		where: {
+			username: req.body.username,
+		},
+	});
+
+	if (!user) {
+		res.status(401);
+		res.json({
+			message: 'Invalid Password or Username!',
+		});
+		return;
+	}
+
+	const isValid = comparePassword(req.body.password, user.password);
+	if (!isValid) {
+		res.status(401);
+		res.json({
+			message: 'Invalid Password or Username!',
+		});
+		return;
+	}
+
+	const token = createJWT(user);
+	res.json({ token });
+};
